442cd2809418f1afe869a7fb06be966145f42715
d954mas <d954mas@gmail.com>
2020-04-14 22:52:39 +0300

    initial

---
+++ sound/sound.go
@@ -0,0 +1,37 @@
+components {
+  id: "sound1"
+  component: "/sound/sound.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sound"
+  type: "sound"
+  data: "sound: \"/sound/example.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
